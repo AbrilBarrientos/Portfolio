642df40d332c6ef5a2d3763ab48539020d4b80d9
Abril Barrientos <153176398+AbrilBarrientos@users.noreply.github.com>
2024-12-05 04:59:11 -0300

    commit doceetwelvee

--- src/components/DispoTag.tsx
+++ src/components/DispoTag.tsx
@@ -15,7 +15,7 @@ const DispoTag = () => {
 
     return (
         <div
-        className={`p-4 rounded-xl ${getStatusClass(status)} transform transition-all duration-500 ease-in-out
+        className={`p-3 rounded-xl ${getStatusClass(status)} transform transition-all duration-500 ease-in-out
             hover:scale-105 hover:shadow-xl hover:bg-green-450 hover:text-green animate-pulse cursor-pointer`}
         style={{
             display: 'inline-block',
